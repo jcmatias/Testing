a4689e7ffde4328745022dcf456d66951c43cc1c
jcmatias <jcmatiasp@gmail.com>
2019-09-04 22:36:05 -0700

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,5 +1,33 @@
-class Solution {
-	static void main(String[] args) {
-		
+import java.net.*;  
+import java.io.*;  
+
+class MyClient{  
+	public static void main(String args[])throws Exception {  
+		Socket s = new Socket("localhost", 5555);  
+
+		// InputStream din = s.getInputStream();  
+		OutputStream dout = s.getOutputStream();  
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+		BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		  
+		String str = "";
+		String str2 = "";  
+
+		while(!str.equals("stop")) {  
+			while (din.ready()) {
+				System.out.println(din.readLine());  
+			}
+			str = br.readLine(); 
+			byte[] request = str.getBytes("UTF-8");
+
+			dout.write(request, 0, request.length);  
+			dout.flush();  
+			System.out.println("Server says: " + din.readLine());  
+
+		}  
+		  
+		dout.close();  
+		s.close();  
 	}
-}
+}  
